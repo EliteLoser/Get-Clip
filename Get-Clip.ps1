cb662a6d4693c69bedc11f88f9c15466451cd065
Joakim Svendsen <joakimbs@gmail.com>
2017-05-01 04:40:08 +0200

    Adding Get-Clip.ps1 script file

---
+++ Get-Clip.ps1
@@ -0,0 +1,15 @@
+﻿function Get-Clip {
+    param([Switch] $Raw)
+    Add-Type -AssemblyName System.Windows.Forms
+    $TextBox = New-Object -TypeName System.Windows.Forms.TextBox
+    $TextBox.Multiline = $true
+    $TextBox.Paste()
+    if ($Raw) {
+        $TextBox.Text
+    }
+    else {
+        # Return array split on newlines.
+        @($TextBox.Text -split '\r?\n')
+    }
+    $TextBox.Dispose()
+}
