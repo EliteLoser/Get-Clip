86cd36b2fa704fc608530a89cac32041b33978bb
Joakim Svendsen <joakimbs@gmail.com>
2017-08-09 03:02:59 +0200

    Add some useful parameters. PSv2 required now.
    See the previous version for a v1-compatible version (hope you don't need it). I could have thrown an if statement at it instead of using parameter sets, but v1 is likely extremely rare now, certainly on a computer where people want a Get-ClipBoard function, so meh.

--- Get-Clip.ps1
+++ Get-Clip.ps1
@@ -1,5 +1,12 @@
-﻿function Get-Clip {
-    param([Switch] $Raw)
+#requires -version 2
+function Get-Clip {
+    [CmdletBinding(DefaultParameterSetName="Cooked?")]
+    param(
+        [Parameter(ParameterSetName="Raw")][Switch] $Raw,
+        [Parameter(ParameterSetName="Cooked?")][Switch] $RemoveEmpty,
+        [Parameter(ParameterSetName="Cooked?")][Switch] $RemoveWhitespaceOnly,
+        [Parameter(ParameterSetName="Cooked?")][Switch] $Quote,
+        [Parameter(ParameterSetName="Cooked?")][String] $QuoteChar = '"')
     Add-Type -AssemblyName System.Windows.Forms
     $TextBox = New-Object -TypeName System.Windows.Forms.TextBox
     $TextBox.Multiline = $true
@@ -8,8 +15,16 @@
         $TextBox.Text
     }
     else {
-        # Return array split on newlines.
-        @($TextBox.Text -split '\r?\n')
+        # Return array split on newlines and filter if switches were passed.
+        if ($RemoveEmpty) {
+            @($TextBox.Text -split '\r?\n') | Where-Object { $_ } | ForEach-Object { if ($Quote) { $QuoteChar + $_ + $QuoteChar } else { $_ } }
+        }
+        elseif ($RemoveWhitespaceOnly) {
+            @($TextBox.Text -split '\r?\n') | Where-Object { $_ -match '\S' } | ForEach-Object { if ($Quote) { $QuoteChar + $_ + $QuoteChar } else { $_ }  }
+        }
+        else {
+            @($TextBox.Text -split '\r?\n') | ForEach-Object { if ($Quote) { $QuoteChar + $_ + $QuoteChar } else { $_ } }
+        }
     }
     $TextBox.Dispose()
 }
